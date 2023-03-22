30eee4dd051435af963c038a3fc29eaa1e6eb959
mineblock11 <93472213+mineblock11@users.noreply.github.com>
2023-03-22 21:54:44 +0000

    1.1.0+1.19.3

--- src/main/java/mine/block/illagerslovearmor/IllagersLoveArmor.java
+++ src/main/java/mine/block/illagerslovearmor/IllagersLoveArmor.java
@@ -3,14 +3,15 @@ package mine.block.illagerslovearmor;
 import mine.block.illagerslovearmor.loot_tables.ILATables;
 import mine.block.illagerslovearmor.loot_tables.RaidWaveCondition;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 
 public class IllagersLoveArmor implements ModInitializer {
     public static final String MODID = "illagerslovearmor";
     @Override
     public void onInitialize() {
         ILATables.init();
-        Registry.register(Registry.LOOT_CONDITION_TYPE, new Identifier(MODID, "wave"), RaidWaveCondition.WAVE);
+        Registry.register(Registries.LOOT_CONDITION_TYPE, new Identifier(MODID, "wave"), RaidWaveCondition.WAVE);
     }
 }
